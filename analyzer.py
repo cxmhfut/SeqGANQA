a88ff72904aeea0fb649e6ae501039517d5b14d9
cxmhfut <912750125@qq.com>
2018-07-24 10:25:16 +0800

    update

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,11 @@
 import re
 
-file_post = open('data/small/weibo_pair_train_Q.post', 'rb')
-file_resp = open('data/small/weibo_pair_train_Q.response', 'rb')
+file_post = open('data/train_Q.post', 'r')
+file_resp = open('data/train_Q.response', 'r')
 
 symbol = {}
 for line in file_post.readlines():
-    line = line.decode('utf-8')[:-1]
+    line = line[:-1]
     for word in re.split(r' ', line):
         if word in symbol:
             symbol[word] += 1
@@ -13,27 +13,27 @@ for line in file_post.readlines():
             symbol[word] = 1
 
 for line in file_resp.readlines():
-    line = line.decode('utf-8')[:-1]
+    line = line[:-1]
     for word in re.split(r' ', line):
         if word in symbol:
             symbol[word] += 1
         else:
             symbol[word] = 1
 
-file_result = open('result.txt', 'wb')
+file_result = open('result.txt', 'w')
 
 total = sum([x[1] for x in symbol.items()])
 num = 0
 word_num = 0
 
-file_result.write(b'<go>\n<eos>\n<unk>\n<pad>\n')
+file_result.write('<go>\n<eos>\n<unk>\n<pad>\n')
 
 for item in sorted(symbol.items(), key=lambda x: x[1], reverse=True):
     print(str([item[0].encode('utf-8')]))
-    file_result.write(item[0].encode('utf-8') + b'\n')
-    num+=item[1]
-    word_num+=1
-    if num>=total*0.99:
+    file_result.write(item[0] + '\n')
+    num += item[1]
+    word_num += 1
+    if num >= total * 0.99:
         break
 
 print(word_num)
---
+++ data_helpers.py
@@ -0,0 +1,76 @@
+import os
+import pickle
+import string
+from tqdm import tqdm
+
+padToken, goToken, eosToken, unknownToken = 0, 1, 2, 3
+dataset_filename = 'data/dataset-70425-vocabSize33050.pkl'
+
+
+def loadDataset(filename):
+    '''
+    读取样本数据
+    :param filename: 文件路径，是一个字典，包含word2id、id2word分别是单词与索引对应的字典和反序字典，
+                    trainingSamples样本数据，每一条都是QA对
+    :return: word2id, id2word, trainingSamples
+    '''
+    dataset_path = os.path.join(filename)
+    print('Loading dataset from {}'.format(dataset_path))
+    with open(dataset_path, 'rb') as handle:
+        data = pickle.load(handle)  # Warning: If adding something here, also modifying saveDataset
+        word2id = data['word2id']
+        id2word = data['id2word']
+        trainingSamples = data['trainingSamples']
+    return word2id, id2word, trainingSamples
+
+
+def sequence2Str(sequence, id2word, clean=False, reverse=False):
+    if not sequence:
+        return ''
+    if not clean:
+        return ''.join([id2word[idx] for idx in sequence])
+    sentence = []
+    for idx in sequence:
+        if idx == eosToken:
+            break
+        elif idx != goToken and idx != padToken:
+            if idx in id2word:
+                sentence.append(id2word[idx])
+            else:
+                sentence.append('<unk>')
+    if reverse:
+        sentence.reverse()
+
+    return detokenize(sentence)
+
+
+def detokenize(tokens):
+    """Slightly cleaner version of joining with spaces.
+    Args:
+        tokens (list<string>): the sentence to print
+    Return:
+        str: the sentence
+    """
+    return ''.join([
+        ' ' + t if not t.startswith('\'') and
+                   t not in string.punctuation
+        else t for t in tokens]).strip().capitalize()
+
+
+def build_dataset():
+    word2id, id2word, trainingSamples = loadDataset(dataset_filename)
+    file_post = open('data/train_Q.post', 'w', encoding='utf-8')
+    file_resp = open('data/train_Q.response', 'w', encoding='utf-8')
+
+    for sample in tqdm(trainingSamples,desc='Building Dataset:'):
+        post = sequence2Str(sample[0], id2word, clean=True) + '\n'
+        response = sequence2Str(sample[1], id2word, clean=True) + '\n'
+        file_post.write(post)
+        file_resp.write(response)
+
+    file_post.close()
+    file_resp.close()
+
+
+if __name__ == '__main__':
+    build_dataset()
